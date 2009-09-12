a4d3b70b55793e53f0d677aec1a0b49f656318c3
jean-mat Grimaldi <jeanmat.grimaldi@gmail.com>
2009-09-12 16:30:51 +0000

    Implementation with base config.js is done. To do : - control panel options - browser (plone_finder in progress) - uploader (based on flashupload)
    git-svn-id: https://svn.plone.org/svn/collective/collective.ckeditor/trunk@97385 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/ckeditor/browser/ckeditorview.py
+++ collective/ckeditor/browser/ckeditorview.py
@@ -1,21 +1,18 @@
 from Acquisition import aq_inner
 from zope.interface import implements, Interface
 from zope.app.component.hooks import getSite
-
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 from collective.ckeditor import siteMessageFactory as _
 
+from collective.ckeditor import LOG
+
 class ICKeditorView(Interface):
     """
     CKeditor browser view interface
     """
-    def contentUsesCKeditor():
-        """return True or False when CKeditor can be used or not"""    
 
-
-    
 class CKeditorView(BrowserView):
     """
     CKeditor browser view
@@ -25,17 +22,22 @@ class CKeditorView(BrowserView):
     def __init__(self, context, request):
         self.context = context
         self.request = request    
-        self.portal = getSite()
+        self.portal = getSite()        
         request.set('ckLoaded',True)
         
     def _memberUsesCKeditor(self):
         """return True if member uses CKeditor"""
         pm = getToolByName(self.portal, 'portal_membership')
         return pm.getAuthenticatedMember().getProperty('wysiwyg_editor')=='CKeditor'
+        
+    def tototest(self):
+        """return True if member uses CKeditor"""
+        pm = getToolByName(self.portal, 'portal_membership')
+        return pm.getAuthenticatedMember().getProperty('wysiwyg_editor')=='CKeditor'        
     
     def contentUsesCKeditor(self, fieldname=''):
         """
-        data for view
+        return True if content uses CKeditor
         """        
         context= aq_inner(self.context)   
         request = self.request 
@@ -44,6 +46,9 @@ class CKeditorView(BrowserView):
                 return True
             if not hasattr(context, 'getField'):
                 return True    
+            field = context.getField(fieldname)
+            if not field:
+                return True
             text_format = request.get('%s_text_format' % fieldname, context.getContentType(fieldname))
             content = field.getEditAccessor(context)()
             try:
