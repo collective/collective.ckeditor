b65177a3c8ac0024d1c28bf04b9b9d8ad35d95ea
jean-mat Grimaldi <jeanmat.grimaldi@gmail.com>
2009-09-27 15:32:08 +0000

    some basic doc tests added
    git-svn-id: https://svn.plone.org/svn/collective/collective.ckeditor/trunk@98293 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/ckeditor/tests/base.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+## Copyright (C) 2009 Ingeniweb - all rights reserved    
+
+"""Base class for ckeditor test cases.
+"""
+
+import sys
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.Five.testbrowser import Browser
+from Products.CMFCore.utils import getToolByName
+from Products.PloneTestCase.layer import PloneSite
+from Testing import ZopeTestCase as ztc
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_product():
+    """
+    Set up the package and its dependencies.
+    """    
+    
+    fiveconfigure.debug_mode = True
+    import collective.ckeditor
+    zcml.load_config('configure.zcml',collective.ckeditor)
+    #fiveconfigure.debug_mode = False    
+    ztc.installPackage('collective.ckeditor')    
+
+setup_product()
+ptc.setupPloneSite(products=['collective.ckeditor'])
+
+
+class CKEditorTestCase(ptc.FunctionalTestCase):
+    """base test case with convenience methods for all ckeditor tests""" 
+    
+    def afterSetUp(self):
+        super(CKEditorTestCase, self).afterSetUp()        
+        self.browser = Browser()        
+        self.uf = self.portal.acl_users
+        self.uf.userFolderAddUser('root', 'secret', ['Manager'], [])        
+        self.ptool = getToolByName(self.portal, 'portal_properties')    
+    
+    def loginAsManager(self, user='root', pwd='secret'):
+        """points the browser to the login screen and logs in as user root with Manager role."""
+        self.browser.open('http://nohost/plone/')
+        self.browser.getLink('Log in').click()
+        self.browser.getControl('Login Name').value = user
+        self.browser.getControl('Password').value = pwd
+        self.browser.getControl('Log in').click()
+    
+    class layer(PloneSite):
+        @classmethod
+        def setUp(cls):
+            # doctests don't play nicely with ipython
+            try :
+                iphook = sys.displayhook
+                sys.displayhook = sys.__displayhook__
+            except:
+                pass    
+
+        @classmethod
+        def tearDown(cls):
+            try :
+                 sys.displayhook = iphook
+            except:
+                pass    
+
---
+++ collective/ckeditor/tests/test_doctests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+## Copyright (C) 2009 Ingeniweb - all rights reserved    
+
+from zope.testing import doctest
+from unittest import TestSuite, main
+from Testing import ZopeTestCase as ztc
+from collective.ckeditor.tests.base import CKEditorTestCase
+
+
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+def test_suite():
+    tests = [ 'installation.txt',
+              'controlpanel.txt',
+             ]
+    suite = TestSuite()
+    for test in tests:
+        suite.addTest(ztc.FunctionalDocFileSuite(test,
+            optionflags=OPTIONFLAGS,
+            package="collective.ckeditor.tests",
+            test_class=CKEditorTestCase))
+    return suite
+
