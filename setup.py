cec769b3ab7a4f8e354638d4cf5dc834a2e1e20d
jean-mat Grimaldi <jeanmat.grimaldi@gmail.com>
2009-12-27 16:56:02 +0000

    new version for ckeditor 3 (current is 3.0.2)
    git-svn-id: https://svn.plone.org/svn/collective/collective.ckeditor/trunk@107528 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ version = str(version).strip()
 
 setup(name='collective.ckeditor',
       version=version,
-      description="CKeditor for Plone integration",
+      description="CKeditor for Plone",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
