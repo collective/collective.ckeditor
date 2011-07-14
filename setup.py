9695105b80a2cbffbef1840b289332bb39b4ea09
Kim Paulissen <spereverde@gmail.com>
2011-07-14 10:01:52 +0000

    Preparing release 3.6.1rc
    git-svn-id: https://svn.plone.org/svn/collective/collective.ckeditor/trunk@242382 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 
 import os
 from setuptools import setup, find_packages
-from xml.dom import minidom
 
-metadata_file = os.path.join(os.path.dirname(__file__),
-                             'collective', 'ckeditor',
-                             'profiles', 'default', 'metadata.xml')
-                             
-metadata = minidom.parse(metadata_file)
-version = metadata.getElementsByTagName("version")[0].childNodes[0].nodeValue
-version = str(version).strip()
+version = '3.6.1rc'
 
 setup(name='collective.ckeditor',
       version=version,
       description="CKEditor for Plone",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
-                       open(os.path.join("docs", "FAQ.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
+                       + open(os.path.join("docs", "FAQ.txt")).read(),
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
