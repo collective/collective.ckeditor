31d705b919c351a34b7e74cbb0232d989528a326
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-07-20 15:18:59 +0200

    fix plugins migration borken never done by Kai Hoppert

--- collective/ckeditor/upgrades.py
+++ collective/ckeditor/upgrades.py
@@ -1,7 +1,18 @@
 from Products.CMFCore.utils import getToolByName
 PROFILE = "profile-collective.ckeditor:default"
 
-
 def common_upgrade(context):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE)
+
+
+def up3411(context):
+    setup = getToolByName(context, 'portal_setup')
+    ptool = getToolByName(context, 'portal_properties')
+    props = ptool.ckeditor_properties
+    if not props.hasProperty('plugins'):
+        props.manage_addProperty(
+            'plugins',
+            ["ajaxsave;/++resource++cke_ajaxsave/plugin.js"],
+            'list')
+
