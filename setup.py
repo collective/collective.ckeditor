8b5e0cbe525097e085455d53e0124de8c3fbbd7a
Godefroid Chapelle <gotcha@bubblenet.be>
2013-09-05 14:06:32 +0200

    pep8

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import os
 from setuptools import setup, find_packages
 
@@ -14,13 +13,18 @@ if not os.path.exists(browser_ckeditor_dir):
         "Check README for more details.".format(browser_ckeditor_dir)
     )
 
-setup(name='collective.ckeditor',
-      version=version,
-      description="CKEditor for Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
-                       + open(os.path.join("docs", "FAQ.txt")).read(),
-      classifiers=[
+long_description = (
+    open("README.rst").read() + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
+    + open(os.path.join("docs", "FAQ.txt")).read()
+)
+
+setup(
+    name='collective.ckeditor',
+    version=version,
+    description="CKEditor for Plone",
+    long_description=long_description,
+    classifiers=[
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
@@ -33,32 +37,31 @@ setup(name='collective.ckeditor',
         "Framework :: Plone :: 4.3",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        ],
-      keywords='Plone CKeditor WYSIWYG',
-      author='Alterway Solutions',
-      author_email='toutpt@gmail.com',
-      url='https://github.com/collective/collective.ckeditor',
-      license='GPL',
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.i18nmessageid',
-          'collective.quickupload',
-          'collective.plonefinder',
-          'plone.app.uuid',
-          'demjson',
-          # -*- Extra requirements: -*-
-      ],
-      packages = find_packages('src'),
-      package_dir = {'': 'src'},
-      extras_require={
-          'test': ['plone.app.testing', 'Products.PloneTestCase', ],
-      },
-      entry_points={
+    ],
+    keywords='Plone CKeditor WYSIWYG',
+    author='Alterway Solutions',
+    author_email='toutpt@gmail.com',
+    url='https://github.com/collective/collective.ckeditor',
+    license='GPL',
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.i18nmessageid',
+        'collective.quickupload',
+        'collective.plonefinder',
+        'plone.app.uuid',
+        'demjson',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    extras_require={
+        'test': ['plone.app.testing', 'Products.PloneTestCase', ],
+    },
+    entry_points={
         'console_scripts': [
-               'copy_ckeditor_code = collective.ckeditor.utils.base2zope:main',
+            'copy_ckeditor_code = collective.ckeditor.utils.base2zope:main',
         ],
         'zest.releaser.releaser.after_checkout': [
             'prepare = collective.ckeditor.utils.base2zope:tag_entrypoint',
@@ -66,5 +69,5 @@ setup(name='collective.ckeditor',
         'z3c.autoinclude.plugin': [
             'target = plone',
         ],
-      },
-      )
+    },
+)
