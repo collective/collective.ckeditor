ccc625a460c8c64da87d91d1463238f4d659d11f
Peter Seifert <peter.seifert1@tu-dresden.de>
2022-09-19 15:15:25 +0200

    WEBCMS-4277 use target title for link text when working with uids
    - Utilize new link text field and automatically populate it.By extending and renaming `convert_uid_to_url` to `show_object_info` we now can now get title of picked object. The text field is populated, whenever it's empty or has been populated with a picked object's title.
    - Make `show_object_info` fail-safe for non-existing objects.
    - WEBCMS-1861: Fix problem that overrides the link text, that has been selected before entering the link dialog.

--- src/collective/ckeditor/browser/statics/ckeditor_plone.js
+++ src/collective/ckeditor/browser/statics/ckeditor_plone.js
@@ -71,74 +71,81 @@ launchCKInstances = function (ids_to_launch) {
     jQuery('.ckeditor_plone').each(function(){
         ckid = jQuery(this).attr('id');
         ids_to_launch = ids_to_launch || [];
-	/* we can specify an array of ids for wich CKeditor has to be launched */
-	/* if no ids is provided or if the current id is in the array of given ids, we proceed */
+        /* we can specify an array of ids for wich CKeditor has to be launched */
+        /* if no ids is provided or if the current id is in the array of given ids, we proceed */
         if ((typeof(ids_to_launch[0]) == 'undefined') || (jQuery.inArray(ckid, ids_to_launch) >= 0)) {
-        cke_config_url = jQuery('.cke_config_url', jQuery(this).parent()).val();
-        var widget_config = {};
-        widget_config.customConfig = cke_config_url;
-        /* Here starts the local js overload of settings by a field widget */
-        if (jQuery('.cke_iswidget', jQuery(this).parent()).length) {
-            // overridden boolean settings need to be converted to boolean again
-            var booleanWidgetSettings = ['allowedContent'];
-            settings = jQuery('.widget_settings input', jQuery(this).parent());
-            settings.each(function () {
-                setting = jQuery(this);
-                var name = setting.attr('class');
-                var value = setting.val();
-                if (jQuery.inArray(name, booleanWidgetSettings) != -1) {
-                    value = value == 'true'
-                }
-                widget_config[name] = value;
-            });
-	    /* Destroy instance if it exists because an existing instance can not be managed twice */
-	    if (typeof CKEDITOR.instances != 'undefined') {
-	        var instance = CKEDITOR.instances[ckid];
-                if (instance) { instance.destroy(true); }
+            cke_config_url = jQuery('.cke_config_url', jQuery(this).parent()).val();
+            var widget_config = {};
+            widget_config.customConfig = cke_config_url;
+            /* Here starts the local js overload of settings by a field widget */
+            if (jQuery('.cke_iswidget', jQuery(this).parent()).length) {
+                // overridden boolean settings need to be converted to boolean again
+                var booleanWidgetSettings = ['allowedContent'];
+                settings = jQuery('.widget_settings input', jQuery(this).parent());
+                settings.each(function () {
+                    setting = jQuery(this);
+                    var name = setting.attr('class');
+                    var value = setting.val();
+                    if (jQuery.inArray(name, booleanWidgetSettings) != -1) {
+                        value = value == 'true'
+                    }
+                    widget_config[name] = value;
+                });
+                /* Destroy instance if it exists because an existing instance can not be managed twice */
+                if (typeof CKEDITOR.instances != 'undefined') {
+                    var instance = CKEDITOR.instances[ckid];
+                        if (instance) { instance.destroy(true); }
+	            };
+            }
+            CKEDITOR.replace( ckid, widget_config);
 	    };
-        }
-        CKEDITOR.replace( ckid, widget_config);
-	};
-    })
-}
+    });
+};
 
 jQuery(document).ready(launchCKInstances);
 
 (function() {
-
-    var format = function format(msg) {
-        return '<p>Actual URL</p><p>'+msg+'</p>';
-    };
-
-    var showActualUrl = function showActualUrl(domElement, protocol, current_uid) {
-        domElement.setHtml(format('Loading...'));
-        if (protocol == 'resolveuid/' && current_uid) {
-            var new_url = CKEDITOR_PLONE_PORTALPATH + '/convert_uid_to_url/' + current_uid;
-            var settings = {
-                url: new_url,
-                type: 'GET',
-                success: function (data, textStatus, jqXHR) {
-                    if (jqXHR.status == 200) {
-                        domElement.setHtml(format(data));
-                    } else {
-                        domElement.setHtml(format('Could not be resolved.'));
-                    }
-                },
-                error: function(jqXHR, textStatus){
-                    domElement.setHtml(format('Could not be resolved.'));
-                }
-            };
-            $.ajax(settings);
-        } else {
-            domElement.setHtml('<p></p>');
-        }
-    };
 
 CKEDITOR.on( 'dialogDefinition', function( ev ) {
     // Take the dialog name and its definition from the event
     // data.
     var dialogName = ev.data.name;
     var dialogDefinition = ev.data.definition;
+
+    if (!ev.editor._getObjectInfoUrl) {
+        ev.editor._getObjectInfoUrl = function(current_uid) {
+            return CKEDITOR_PLONE_PORTALPATH + '/show_object_info/' + current_uid
+        }
+    }
+
+    var showObjectInfo = function showObjectInfo(domElement, urlTextElement, protocol, current_uid) {
+            function format(msg) {
+                return '<p>Actual URL:</p><p>'+msg+'</p>';
+            }
+
+            domElement.setHtml(format('Loading...'));
+            if (protocol == 'resolveuid/' && current_uid) {
+                $.ajax({
+                    url: ev.editor._getObjectInfoUrl(current_uid),
+                    dataType: 'json',
+                    method: 'GET',
+                    success: function (data, textStatus, jqXHR) {
+                        var resolved_url = data.url,
+                            resolved_title = data.title;
+                        domElement.setHtml(format(resolved_url));
+                        if (urlTextElement && (urlTextElement.getValue() == '' || !urlTextElement.dirty)) {
+                            urlTextElement.setValue(resolved_title);
+                            urlTextElement.dirty = false;
+                        }
+                    },
+                    error: function (jqXHR, textStatus) {
+                        domElement.setHtml(format('Could not be resolved.'));
+                    }
+                });
+            } else {
+                domElement.setHtml('<p></p>');
+            }
+    };
 
     // Check if the definition is from the dialog we're
     // interested on (the "Link" dialog).
@@ -159,7 +166,7 @@ CKEDITOR.on( 'dialogDefinition', function( ev ) {
                 var domElement = this.getElement();
                 // Since we can't hook into the plugin's url parsing, we have to do it here.
                 if (data.url && !data.url.protocol && data.url.url.indexOf('resolveuid/') == 0) {
-                    showActualUrl(domElement, 'resolveuid/', data.url.url.split('resolveuid/')[1]);
+                    showObjectInfo(domElement, null, 'resolveuid/', data.url.url.split('resolveuid/')[1]);
                 } else {
                     domElement.setHtml('<p></p>');
                 }
@@ -184,6 +191,7 @@ CKEDITOR.on( 'dialogDefinition', function( ev ) {
                     protocolValue = protocolField.getValue(),
                     urlField = this.getDialog().getContentElement('info', 'url'),
                     urlValue = urlField.getValue(),
+                    linkTextField = this.getDialog().getContentElement('info', 'linkDisplayText'),
                     actualField = this.getDialog().getContentElement('info', 'actual'),
                     actualElement = actualField.getElement(),
                     additionalProtocol;
@@ -199,9 +207,23 @@ CKEDITOR.on( 'dialogDefinition', function( ev ) {
                     }
                 }
                 if (protocolValue == 'resolveuid/') { // Update url as well
-                    showActualUrl(actualElement, protocolValue, urlValue);
+                    showObjectInfo(actualElement, linkTextField, protocolValue, urlValue);
                 }
                 this.allowOnChange = true;
+            }
+        });
+
+        // Mark field, if it has not been populated by object's data.
+        var linkTextField = infoTab.get('linkDisplayText');
+        linkTextField.onKeyUp = function(ev) {
+            this.dirty = true;
+        };
+        linkTextField.setup = CKEDITOR.tools.override(linkTextField.setup, function(org) {
+            return function (data) {
+                org.apply(this, [data]);
+                if (this.getValue()) {
+                    this.dirty = true;
+                }
             }
         });
 
