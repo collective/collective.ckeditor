ec1a1b90c420f26fb1398f465d41f55106a85bd1
Godefroid Chapelle <gotcha@bubblenet.be>
2023-11-06 16:58:00 +0100

    poc

---
+++ src/collective/ckeditor/browser/plonepicturevariants/lang/fr-ca.js
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'plonepicturevariants', 'fr-ca', {
+	alt: 'Texte alternatif',
+	btnUpload: 'Envoyer sur le serveur',
+	captioned: 'Captioned image', // MISSING
+	captionPlaceholder: 'Caption', // MISSING
+	infoTab: 'Informations sur l\'plonepicturevariants',
+	lockRatio: 'Verrouiller les proportions',
+	menu: 'Propriétés de l\'plonepicturevariants',
+	pathName: 'image', // MISSING
+	pathNameCaption: 'caption', // MISSING
+	resetSize: 'Taille originale',
+	resizer: 'Click and drag to resize', // MISSING
+	title: 'Propriétés de l\'plonepicturevariants',
+	uploadTab: 'Téléverser',
+	urlMissing: 'L\'URL de la source de l\'image est manquant.',
+	altMissing: 'Alternative text is missing.' // MISSING
+} );
